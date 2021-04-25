c2d8acba4f2ba7012a6136e0e3c0081af8aecbc2
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-04-25 11:55:12 +0900

    commit

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -4,8 +4,8 @@ import {Answer} from  './index'
 const AnswersList = (props) => { //ダミーデータを入れた方が開発効率が上がる
   return(
     <div className= "c-grid__answer">
-      {props.answers.map((value, index) => {
-        return <Answer content={value.content} />
+      {props.answers.map((value, index) => { //繰り返しの構文mapメソッド。
+        return <Answer content={value.content} key={index.toString()} //このkey以降を書かないとコンソールエラーが出てしまう>
       })}
 
     </div>
