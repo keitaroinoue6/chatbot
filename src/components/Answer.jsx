0a0563368195b9e9ee77faa1260bf501fc07bae0
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-06 22:40:24 +0900

    Hook API の導入

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-
-}));
+const useStyles = makeStyles(() => (
+  createStyles({
+    "button": {
+      borderColor: "#FF8549",
+      color: "FF8549",
+      fontWeight: 600,
+      marginBottom: "8px",
+      "&:hover": {
+        backgroundColor: "#FF8549",
+        color: "#fff"
+      }
+    }
+  })
+));
 
 
 const Answer = (props) => {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return ( //選択された時に次どの質問に行くのかというnextIdというキーがある
-    <Button variant="contained" color="primary" onClick={() => props.select(props.content,props.nextId)}> 
+    <Button
+      className={classes.button} //上記のcreateStylesの箇所で
+      variant="contained" color="primary" onClick={() => props.select(props.content,props.nextId)}
+    > 
       {props.content}
     </Button>
   )
